f919847d1c741dca889d3ba0fe0301698cc73b20
MarcusDo <167722885+MarcusDo@users.noreply.github.com>
2025-11-02 18:21:36 +1100

    Create todo-test.py

---
+++ .github/scripts/todo-test.py
@@ -0,0 +1,24 @@
+import unittest
+from todo import Task, TaskPool
+
+class TestTaskPool(unittest.TestCase):
+
+    def setUp(self):
+        self.pool = TaskPool()
+
+    def test_add_task(self):
+        task = Task("New Task")
+        self.pool.add_task(task)
+        self.assertEqual(len(self.pool.tasks), 1)
+
+    def test_get_open_tasks(self):
+        self.pool.populate()
+        open_tasks = self.pool.get_open_tasks()
+        self.assertEqual(len(open_tasks), 3)
+        self.assertTrue(all(task.status == "ToDo" for task in open_tasks))
+
+    def test_get_done_tasks(self):
+        self.pool.populate()
+        done_tasks = self.pool.get_done_tasks()
+        self.assertEqual(len(done_tasks), 3)
+        self.assertTrue(all(task.status == "Done" for task in done_tasks))
